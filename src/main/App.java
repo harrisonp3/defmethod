5a3cf445d58e9dc49df041eedf9fa4f35f929d6f
HARRISON <harrison.paup@gmail.com>
2018-12-03 20:00:30 -0500

    added jars to bin

--- src/main/App.java
+++ src/main/App.java
@@ -8,12 +8,12 @@ public class App {
 	public App() {}
 
 	public static void main(String[] args) throws FileNotFoundException, ParseException {
-//		if (args.length == 0 || args[0] == null || args[0].trim().isEmpty()) {
-//			System.out.println("Enter a filepath!");
-//			return;
-//		}
-//		String path = args[0];
-		String path = "./src/main/pipe.txt";
+		if (args.length == 0 || args[0] == null || args[0].trim().isEmpty()) {
+			System.out.println("Enter a filepath!");
+			return;
+		}
+		String path = args[0];
+//		String path = "./src/main/pipe.txt";
 		InputParser parser = new InputParser();
 		parser.setScanner(path);
 		parser.parse();
